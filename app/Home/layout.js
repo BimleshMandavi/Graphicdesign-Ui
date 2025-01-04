4a9e342872c0c34b20bbe7be4946ac4d96859b7c
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2025-01-04 12:45:07 +0530

    commit

--- app/Home/layout.js
+++ app/Home/layout.js
@@ -1,5 +1,5 @@
 // /app/layout.js
-import Navbar from "@/components/Navibar"; // Assuming Navbar is in components folder
+import Navbar from "../../components/Navibar";
 export default function Layout({ children }) {
   return (
     <div>
